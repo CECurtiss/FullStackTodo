abc3f5743cabbb45957528d875c53c9f6b2e0498
CECurtiss <curtissclinton@gmail.com>
2025-11-30 14:39:14 -0600

    created a small local json file to serve as temporary db. created a backend route for items, commented out local routes in item.ts, working on getItems route

--- BackendFlask/app.py
+++ BackendFlask/app.py
@@ -1,7 +1,15 @@
 from flask import Flask
+import json
+from markupsafe import escape
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route('/items')
+def get_items():
+    with open('db.json') as f:
+        data = json.load(f)
+    return data
--- FrontEndAngular/src/app/services/item.ts
+++ FrontEndAngular/src/app/services/item.ts
@@ -1,60 +1,36 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Item } from '../models/item.model';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ItemService {
-  // private apiUrl = 'http://localhost:5000';
-  // constructor(private http: HttpClient) {}
-    private items: Item[] = [
-      {
-         id: 1, 
-         priority: 'High', 
-         task: 'Finish project', 
-         dueDate: new Date('2024-06-30'), 
-         completed: false 
-        },
-       { 
-        id: 2, 
-        priority: 'Medium', 
-        task: 'Buy groceries', 
-        dueDate: new Date('2024-07-02'), 
-        completed: true, 
-        dateCompleted: new Date('2024-06-25')
-       },
-       { 
-        id: 3, 
-        priority: 'Low', 
-        task: 'Walk the dog', 
-        dueDate: new Date('2024-07-05'), 
-        completed: false
-       }
-     ];
-      constructor() {}
-
-
-   //  get items from backend
-  getItems(): Item[] {
-    return [...this.items];
+  private apiUrl = 'http://localhost:5000';
+  constructor(private http: HttpClient) {}
+    
+  
+  //  get items from backend
+  getItems(): Observable<Item[]> {
+    return this.http.get<Item[]>(this.apiUrl)
   }
+}
   // add item to backend
-addItem(item: Item): void {
-    item.id = this.items.length + 1;
-    this.items.push(item);
-  }
+// addItem(item: Item): void {
+//     item.id = this.items.length + 1;
+//     this.items.push(item);
   // update item in backend
 
 
   // delete item from backend
-  deleteItem(id: number): void {
-    this.items = this.items.filter(item => item.id !== id);
-  }
+  // deleteItem(id: number): void {
+  //   this.items = this.items.filter(item => item.id !== id);
+  // }
 
   // get item by id
-  getItemById(id: number): Item | undefined {
-    return this.items.find(item => item.id === id);
-  }
+  // getItemById(id: number): Item | undefined {
+  //   return this.items.find(item => item.id === id);
+  // }
   
-}
+
