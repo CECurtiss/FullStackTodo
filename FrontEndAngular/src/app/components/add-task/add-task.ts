63e1eacda78aba1b5b9f8a42d269333453d829a4
CECurtiss <curtissclinton@gmail.com>
2025-11-30 10:50:45 -0600

    in add-task.ts. created a function to reset the task to empty fields when initializing and addTask is called. put name properties on html inputs to fix ngModel error. Added pipe to fix date display on submission

--- FrontEndAngular/src/app/components/add-task/add-task.ts
+++ FrontEndAngular/src/app/components/add-task/add-task.ts
@@ -2,36 +2,38 @@ import { Component } from '@angular/core';
 import { Item } from '../../models/item.model';
 import { ItemService } from '../../services/item';
 import { FormsModule } from '@angular/forms';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-add-task',
-  imports: [FormsModule],
+  imports: [FormsModule, DatePipe],
   templateUrl: './add-task.html',
   styleUrl: './add-task.css',
 })
 export class AddTask {
   constructor(private itemService: ItemService) {}
 
-  newTask: Item = {
-    id: 0,
-    priority: '',
-    task: '',
-    dueDate: new Date(),
-    completed: false,
-    dateCompleted: undefined
-  }
-
-  addTask(): void {
-    this.itemService.addItem(this.newTask);
-    this.newTask = {
+  newTask: Item = this.resetTask();
+  
+  private resetTask(): Item {
+    return {
       id: 0,
       priority: '',
       task: '',
       dueDate: new Date(),
       completed: false,
       dateCompleted: undefined
-    };
+    }
   }
 
+  addTask(): void {
+    console.log(this.newTask);
+    this.itemService.addItem(this.newTask);
+    this.newTask = this.resetTask();
+  }
+
+  ngOnInit(): void {
+
+  }
   
 }
